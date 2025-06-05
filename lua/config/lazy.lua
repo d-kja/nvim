9ae257e5adf21dbeab51a1753b9a7ad7ca8a0863
d-kja <89911360+d-kja@users.noreply.github.com>
2025-06-04 22:12:44 -0300

    aa

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
 
     { import = "plugins" },
   },
