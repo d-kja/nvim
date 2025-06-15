d8549866e495b1f846037ba5dec30a0778ae480e
d-kja <89911360+d-kja@users.noreply.github.com>
2025-06-15 00:01:28 -0300

    sdadwa

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,7 +14,7 @@ global.netrw_banner = 0 -- Hide banner
 -- vim.g.netrw_altv = 1 -- Open with right splitting
 -- vim.g.netrw_liststyle = 3 -- Tree-style view
 -- vim.g.netrw_browse_split = 4 -- Open in previous window
--- vim.g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]] -- use .gitignore
+global.netrw_list_hide = [[node_modules,.git,.expo]] -- WARN: temporary
 
 -- Set to true if you have a Nerd Font installed
 global.have_nerd_font = true
